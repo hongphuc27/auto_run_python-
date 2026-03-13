04c3c9d47c810d49bfc6260601c3f41d6c4e6529
Lê Hồng Phúc <honhphuc2711@gmail.com>
2026-03-13 15:57:57 +0700

    Update dtsaleworkbigqueryshopee.py

--- dtsaleworkbigqueryshopee.py
+++ dtsaleworkbigqueryshopee.py
@@ -22,7 +22,7 @@ CHANNEL = "Shopee"
 
 now_utc = datetime.now(timezone.utc)
 
-DATE_FROM = (now_utc - timedelta(days=35)).strftime("%Y-%m-%dT%H:%M:%S.000Z")
+DATE_FROM = (now_utc - timedelta(days=50)).strftime("%Y-%m-%dT%H:%M:%S.000Z")
 DATE_TO = now_utc.strftime("%Y-%m-%dT%H:%M:%S.000Z")
 
 # DATE_FROM = datetime(2026, 1, 1, 0, 0, 0, tzinfo=timezone.utc) \
@@ -170,7 +170,7 @@ def main():
 
     delete_query = f"""
         DELETE FROM `{table_ref}`
-        WHERE createdAt >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 35 DAY)
+        WHERE createdAt >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 50 DAY)
     """
 
     client.query(delete_query).result()
@@ -197,6 +197,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
