8af66395aa5ae840555df8faaf88ef9ad687b815
Lê Hồng Phúc <honhphuc2711@gmail.com>
2026-05-05 23:32:47 +0700

    Update dtsaleworkbigquerytiktok.py

--- dtsaleworkbigquerytiktok.py
+++ dtsaleworkbigquerytiktok.py
@@ -75,7 +75,7 @@ VN_TZ = timezone(timedelta(hours=7))
 
 def build_date_range():
     today_vn = datetime.now(VN_TZ).date()
-    start_day_vn = today_vn - timedelta(days=125)
+    start_day_vn = today_vn - timedelta(days=25)
 
     start_dt_vn = datetime.combine(start_day_vn, datetime.min.time(), tzinfo=VN_TZ)
     end_dt_vn = datetime.combine(
@@ -290,7 +290,7 @@ def build_dataframe(orders):
 # giữ logic xóa theo code 1
 # ========================================
 def delete_last_22_days():
-    start_day_vn = datetime.now(VN_TZ).date() - timedelta(days=125)
+    start_day_vn = datetime.now(VN_TZ).date() - timedelta(days=25)
     start_dt_vn = datetime.combine(start_day_vn, datetime.min.time(), tzinfo=VN_TZ)
 
     delete_query = f"""
