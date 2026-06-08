0f683bd14760e75bca6613c8a768513afda5f8d3
Lê Hồng Phúc <honhphuc2711@gmail.com>
2026-06-08 10:11:16 +0700

    Update keocptiktok.py

--- keocptiktok.py
+++ keocptiktok.py
@@ -37,7 +37,7 @@ HEADERS = {
 # =====================================================
 
 # today = datetime.today().date()
-today = datetime(2026, 6, 5).date()
+today = datetime(2026, 6, 6).date()
 api_date = today.strftime("%Y-%m-%d")
 
 # =====================================================
