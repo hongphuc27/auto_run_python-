50dd466b4dfe92bebb3340436dcc92ed3848e7a5
Lê Hồng Phúc <honhphuc2711@gmail.com>
2026-07-08 14:50:42 +0700

    Update report_linh.py

--- report_linh.py
+++ report_linh.py
@@ -217,8 +217,8 @@ df["So_luong"] = pd.to_numeric(df["So_luong"], errors="coerce").astype("Int64")
 # =========================
 # DEFINE CUTOFF (25 NGÀY)
 # =========================
-cutoff_date = (datetime.today() - timedelta(days=60)).date()
-# cutoff_date = datetime(2026, 1, 1).date()
+# cutoff_date = (datetime.today() - timedelta(days=60)).date()
+cutoff_date = datetime(2026, 6, 16).date()
 print("Start date:", cutoff_date)
 
 # =========================
